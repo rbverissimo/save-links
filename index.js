4cac77e64dc2cd09b46f8c7a215912689f2dd5ba
rbverissimo <rverissimo.dev@gmail.com>
2022-05-24 11:30:27 -0300

    refactorin and making the render function more versatile

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-let myLeads = [];
+let myLinks = [];
 const inputBtn = document.getElementById("input-btn");
 const clearBtn = document.getElementById("clear-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
 window.addEventListener("DOMContentLoaded", function () {
-  if (localStorage.getItem("myLeads") != null) {
-    renderLeads();
+  if (localStorage.getItem("myLinks") != null) {
+    renderLeads(myLinks);
   }
 });
 
 inputBtn.addEventListener("click", function () {
   let data = inputEl.value;
-  if (myLeads == null) myLeads = [];
-  myLeads.push(data);
-  myLeads = localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  if (myLinks == null) myLinks = [];
+  myLinks.push(data);
+  myLinks = localStorage.setItem("myLinks", JSON.stringify(myLinks));
   inputEl.value = "";
-  renderLeads();
+  render(myLinks);
 });
 
 clearBtn.addEventListener("click", function () {
-  localStorage.removeItem("myLeads");
-  renderLeads();
+  localStorage.removeItem("myLinks");
+  render(myLinks);
 });
 
-function renderLeads() {
+function render(myLinks) {
   let listItems = "";
-
-  myLeads = JSON.parse(localStorage.getItem("myLeads"));
-  if (myLeads == null) ulEl.innerHTML = listItems;
+  myLinks = JSON.parse(localStorage.getItem("myLinks"));
+  if (myLinks == null) ulEl.innerHTML = listItems;
   else
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < myLinks.length; i++) {
       listItems += `<li>
-        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+        <a href='${myLinks[i]}' target='_blank'>${myLinks[i]}</a>
                   </li>
         `;
     }
